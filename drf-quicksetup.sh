3ae31f81f0fc278432fc99e2beb277ce1091dd75
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-08-07 18:03:24 +0800

    added functions for creating apps and models, will add serializers in a bit

--- drf-quicksetup.sh
+++ drf-quicksetup.sh
@@ -1,26 +1,145 @@
 #!/usr/bin/env bash
-indent="\ \ \ \ " # not a big fan of this
+# perhaps not the greatest idea i've had
+ind="    " # not a big fan of this
+sind="\ \ \ \ "
 
 blank-make () {
-    echo "This template will only create a project configured for REST and nothing else."
-    echo "Making blank template.." 
-    echo "$PWD"
+    echo "Creating project structure..."
+    mkdir "${1}"
+    cd "${1}/"
+
+    # setup django environment
+    projName="${1}backend"
+    python -m venv ./venv
+    source ./venv/bin/activate
+    pip install django django_rest_framework django-cors-headers
+    django-admin startproject "$projName"
+    cd "$projName"
 
     # i COULD use grep to find the appropriate lines 
     # but why do that when this works too
-    sed -i "40 i ${indent}'corsheaders',\n${indent}'rest_framework'," ./$1/settings.py
-    sed -i "52 i ${indent}'corsheaders.middleware.CorsMiddleware'," ./$1/settings.py
+    # this is gonna come back to haunt me after a couple commits
+    sed -i "40 i ${sind}'corsheaders',\n${sind}'rest_framework'," ./$projName/settings.py
+    sed -i "52 i ${sind}'corsheaders.middleware.CorsMiddleware'," ./$projName/settings.py
 
     echo "Input url for whitelist (leave blank for localhost:3000)"
     read whitelist
 
-    if ["$whitelist" == ""]; then
+    if [ "$whitelist" == "" ]; then
 	whitelist="http://localhost:3000"
     fi
 
     echo "CORS_ORIGIN_WHITELIST = [
-	'$whitelist'
-]" >> ./$1/settings.py
+    '$whitelist'
+]" >> ./$projName/settings.py
+}
+
+mk-app () {
+    echo "input project name"
+    read projName
+
+    if [ ! -f "manage.py" ]; then
+	echo "Not in a valid django project directory root. Exiting.."
+	exit 1
+    fi
+
+    python manage.py startapp $1
+    sed -i "42 i ${sind}'$1'," $projName/settings.py
+
+}
+
+mk-model () {
+    echo "input app name"
+    read appName
+
+    if [ ! -d "${appName}/" ]; then
+	echo "App doesn't exist in django project."
+	exit 1
+    fi
+
+    if [ ! -f "manage.py" ]; then
+	echo "Not in a valid django project directory root. Exiting.."
+	exit 1
+    fi
+
+    echo "these files will be changed, review if they are correct"
+    echo "${appName}/models.py"
+    echo "${appName}/serializers.py"
+
+    read confirm
+    if [[ "$confirm" != "y" ]]; then
+	exit 1
+    fi
+
+    declare -A fields
+    echo "Declare model fields"
+    echo "name type default"
+    echo "types are [int, char, bool, str, date]"
+    echo "\"end\" to stop adding fields"
+
+    while true; do
+	read attr
+	if [[ "$attr" == "end" ]]; then 
+	    break
+	fi
+
+	key=$(echo $attr | awk '{print $1}') 
+	fields+=(["$key"]=$(sed -e 's/^[^ ]* //' <<< "$attr"))
+    done
+
+    local model="class ${1}(models.Model):"
+    local ttype
+    local type
+    local default
+
+    # pair type to field
+    for i in "${!fields[@]}"; do
+	ttype=$(echo ${fields[$i]} | awk '{print $1}')
+	default=$(echo ${fields[$i]} | awk '{print $2}')
+
+	case "$ttype" in
+	    int) 
+		type="IntegerField(default=${default})"
+		;;
+	    char)
+		type="CharField(default=${default}, max_length=100)"
+		;;
+	    bool)
+		type="BooleanField(default=${default})"
+		;;
+	    str)
+		type="TextField(default=${default})"
+		;;
+	    date)
+		type="DateTimeField()"
+		;;
+	    end)
+		break
+		;;
+	    *)
+		echo "invalid type: ${ttype}"
+		echo "Exiting.."
+		exit 1
+		;;
+	esac
+
+	model+="\n${ind}${i} = models.${type}"
+    done
+
+    echo "Review model to be added (y/n to continue)"
+    printf "$model"
+
+    echo ""
+    read confirm
+    if [[ "$confirm" != "y" ]]; then
+	exit 1
+    fi
+
+    printf "$model\n\n" >> $appName/models.py
+
+    echo "making migrations.."
+    python manage.py makemigrations $appName
+    python manage.py migrate
 }
 
 help () {
@@ -43,27 +162,29 @@ while getopts "hn:m:" opts; do
     esac
 done
 
-echo "Creating project structure..."
-mkdir "${name}"
-cd "${name}/"
-
-# setup django environment
-python -m venv ./venv
-source ./venv/bin/activate
-pip install django django_rest_framework django-cors-headers
-django-admin startproject "${name}backend"
-cd "${name}backend"
-
 # setup template
 case "${mode}" in
-    blank) blank-make "${name}backend";;
-    # ecommerce) ecommerce-make $name;;
-    # blog) blog-make $name;;
+    blank)
+	echo "This template will only create a project configured for REST and nothing else."
+	echo "Making blank template.." 
+	blank-make "${name}"
+	echo "Finished setting up a blank template."
+	echo "Setup done."
+	echo "cd to ${name} and source venv to get started."
+	exit 0
+	;;
+    mkmodel)
+	mk-model "$name"
+	echo "Added model ${name}"
+	exit 0
+	;;
+    mkapp)
+	mk-app "$name"
+	echo "App ${name} created and registered"
+	exit 0
+	;;
     *) 
 	echo "not in modes"
-	exit 1
+	exit 0
 	;;
 esac
-
-echo "Setup done."
-echo "cd to ${name} to get started."
